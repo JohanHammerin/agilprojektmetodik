f936c8529ce38fd628ca699a893f8e0256b810ff
johanhammerin <johan04hammerin@gmail.com>
2025-03-26 09:35:29 +0100

    add correct path to pollen-img

--- app/types/pollen-types.ts
+++ app/types/pollen-types.ts
@@ -2,45 +2,45 @@
 
 // Record = Gör varje string (id numret) till en nyckel som sedan hämtar name & img
 
-export const PollenTypes: Record<string, {name: string, img: string}> = {
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323236":{
-      name: "Al", 
-      img: "/public/img-pollenTypes/Al.webp"
-    },
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323533": {
-      name: "Malörtsambrosia",
-      img: "/public/img-pollenTypes/Malmörtssambrosia.webp"
-    },
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323530": {
-      name: "Gråbro",
-      img: "/public/img-pollenTypes/Gråbo.webp"
-    },
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323332": {
-      name: "Björk",
-      img: "/public/img-pollenTypes/Malmörtssambrosia.webp"
-    },
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323233": {
-      name: "Hassel",
-      img: "/public/img-pollenTypes/Hassel.webp"
-    },
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323335": {
-      name: "Bok",
-      img: "/public/img-pollenTypes/Malmörtssambrosia.webp"
-    },
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323433": {
-      name: "Gräs",
-      img: "/public/img-pollenTypes/Malmörtssambrosia.webp"
-    },
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323337": {
-      name: "Ek",
-      img: "/public/img-pollenTypes/Malmörtssambrosia.webp"
-    },
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323330": {
-      name: "Sälj och viden",
-      img: "/public/img-pollenTypes/Malmörtssambrosia.webp"
-    },
-    "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323331": {
-      name: "Alm",
-      img: "/public/img-pollenTypes/Malmörtssambrosia.webp"
-    },
-  };
+export const PollenTypes: Record<string, { name: string; img: string }> = {
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323236": {
+    name: "Al",
+    img: "/img-pollenTypes/Al.webp",
+  },
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323533": {
+    name: "Malörtsambrosia",
+    img: "/img-pollenTypes/Malmörtssambrosia.webp",
+  },
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323530": {
+    name: "Gråbro",
+    img: "/img-pollenTypes/Gråbo.webp",
+  },
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323332": {
+    name: "Björk",
+    img: "/img-pollenTypes/Malmörtssambrosia.webp",
+  },
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323233": {
+    name: "Hassel",
+    img: "/img-pollenTypes/Hassel.webp",
+  },
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323335": {
+    name: "Bok",
+    img: "/img-pollenTypes/Malmörtssambrosia.webp",
+  },
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323433": {
+    name: "Gräs",
+    img: "/img-pollenTypes/Malmörtssambrosia.webp",
+  },
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323337": {
+    name: "Ek",
+    img: "/img-pollenTypes/Malmörtssambrosia.webp",
+  },
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323330": {
+    name: "Sälj och viden",
+    img: "/img-pollenTypes/Malmörtssambrosia.webp",
+  },
+  "2a2a2a2a-2a2a-4a2a-aa2a-2a313a323331": {
+    name: "Alm",
+    img: "/img-pollenTypes/Malmörtssambrosia.webp",
+  },
+};
