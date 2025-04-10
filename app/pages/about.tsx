de38e6ccf6876e73fac133d9ccb0d28bd8679da9
johanhammerin <johan04hammerin@gmail.com>
2025-04-10 11:20:53 +0200

    footer text fix (Hela gruppen)

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -64,7 +64,7 @@ export function AboutPage() {
           </div>
         </section>
       </main>
-      <footer className="footer-desktop">
+      <footer className="footer-desktop-about">
         <h3>&#169;2025 Copyright Pollenkollen | All Rights Reserved</h3>
         <NavLink to="/">Hem</NavLink>
       </footer>
